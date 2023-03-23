c431d64123d1170a03a039cacc5616c1746b4acf
Magnus Atli <magjrgylfason@gmail.com>
2023-03-23 15:45:34 +0000

    new rule test

--- rules.test.js
+++ rules.test.js
@@ -1,28 +1,25 @@
-import { describe, test } from "vitest";
-
-import { rules } from "./no-moment";
-
+import { test } from "vitest";
+import rule from "./no-moment";
 import { RuleTester } from "eslint";
 
-describe("rules", () => {
-
-    test("no-moment", () => {
-        const ruleTest = new RuleTester();
-        ruleTest.run("no-moment", rules, {
-            valid: [
-                'var moment = "something that is not moment";',
-                'var sndMoment = require("not-moment");'
-            ],
-            invalid: [
-                {
-                    code: "var moment = require('moment');",
-                    errors: [
-                        {
-                            message: "Unexpected use of moment()",
-                        },
-                    ],
-                },
-            ],
-        });
-    });
+test("no-moment", () => {
+  const ruleTester = new RuleTester();
+  ruleTester.run("no-moment", rule, {
+    valid: [
+      {
+        code: 'var _ = require("lodash");',
+      },
+    ],
+    invalid: [
+      {
+        code: "var m = moment();",
+        errors: [
+          {
+            message: "Unexpected use of moment()",
+            type: "CallExpression",
+          },
+        ],
+      },
+    ],
+  });
 });
